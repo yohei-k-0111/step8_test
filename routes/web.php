330164b0ae80a2cee496b414ae2bf5af301c80b6
yohei-k-0111 <y.kusumoto727@gmail.com>
2023-12-20 22:28:04 +0900

    retry second commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// "Route"というツールを使うために必要な部品を取り込む。
+use App\Http\Controllers\ProductController;
+// ProductControllerに繋げるために取り込む。
+use Illuminate\Support\Facades\Auth;
+// "Auth"という部品を使うために取り込む。ユーザー認証（ログイン）に関する処理を行う。
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +20,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // ウェブサイトのホームページ（'/'のURL）にアクセスした場合のルート
+    if (Auth::check()) {
+        // ログイン状態ならば商品一覧ページ（ProductControllerのindexメソッドが処理）にリダイレクト
+        return redirect()->route('products.index');
+    } else {
+        // ログイン状態でなければログイン画面へリダイレクト
+        return redirect()->route('login');
+    }
+});
+
+Auth::routes();
+
+// Auth::routes();はLaravelが提供している便利な機能で
+// 一般的な認証に関するルーティングを自動的に定義してくれます
+// この一行を書くだけで、ログインやログアウト
+// パスワードのリセット、新規ユーザー登録などのための
+// ルートが作成されます。
+//　つまりログイン画面に用意されたビューのリンク先がこの1行で済みます
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('products', ProductController::class);
+});
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'products.index'])->name('home');
+
+Route::get('/home', function () {
+    // ウェブサイトのホームページ（'/'のURL）にアクセスした場合のルート
+    if (Auth::check()) {
+        // ログイン状態ならば商品一覧ページ（ProductControllerのindexメソッドが処理）へリダイレクト
+        return redirect()->route('products.index');
+    } else {
+        // ログイン状態でなければログイン画面へリダイレクト
+        return redirect()->route('login');
+    }
 });
